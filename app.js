3a991c81dd1198d64cac28ab8cca3ed3848b386b
Vinith <vinithdevadiga7@gmail.com>
2022-08-16 23:36:18 +0530

    need to fix

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ function checkwrite() {
         userTextground.removeEventListener("keydown", checkwrite);
 
 }
-const keywords = ["A", "B", "C", "D", "E", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+const keywords = [ "D", "E", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
         "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e",
         "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "Backspace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
         "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=",","]
