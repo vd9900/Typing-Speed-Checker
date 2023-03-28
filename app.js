9aef3bcd3f2bfc896cf5448a3d2d5f2f5d27fdf1
vinith <vinithdevadiga7@.com>
2023-03-28 15:43:27 +0530

    now done

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const story = `Sam and Tom were identical twins. They were so identical that even their mother found it difficult to distinguish one from the other, at least during their initial days on earth.However, they were very different from each other when it came to everything other than their appearance. Sam had no friends, while Tom was a great friendship maker. Sam loved sweets, but Tom loved spicy food and detested sweets. Sam was mommy’s pet and Tom was daddy’s pet. While Sam was generous and selfless, Tom was greedy and selfish!
 As Sam and Tom grew up, their father wanted to share his fortune equally amongst them. However, Tom did not agree and he argued that whoever proved to be more intelligent and strong would have to get a bigger share of the wealth.Sam agreed. Their father decided to organize a competition between the two. He asked the two sons to walk as long as they could, and return home before sunset. The wealth would be divided in proportion to the distance covered. As a rule of the competition, they were not permitted to carry a watch to keep track of the time.The following day, Sam and Tom set out to walk. It was a rather sunny day. Sam walked slowly and steadily, while Tom broke into a sprint as he was bent on winning the race and also winning a greater portion of his father’s wealth.Sam knew that it would be ideal to walk as far as possible till noon and start for home at noon as it would take the same amount of time to walk back home. Knowing this, Sam decided to turn back for home at noon so as reach home on time.However, Tom, with his greed to earn more wealth, did not attempt to return home even after mid-noon. He walked twice as long as Sam, and thought he would still be able to return home before sunset. He hurried back when he saw the sun turn orange. Unfortunately, he could not even make it half way home as the sun started to set. Slowly darkness engulfed his path and he had to drag his tired feet back home.
-He had lost the race. Only because of his greed.Greed leads to loss.`
-const storyArray = story.split('. ')
-console.log(storyArray)
+He had lost the race. Only because of his greed.Greed leads to loss.`;
+const storyArray = story.split(". ");
+console.log(storyArray);
 // console.log(storyArray);
-const section1 = document.querySelector(".section1")
-const section2 = document.querySelector(".section2")
-const apiText = document.querySelector(".api__text")
-const textdefult = document.querySelector(".defulttext")
-const praentOption = document.querySelector(".nav__options")
+const section1 = document.querySelector(".section1");
+const section2 = document.querySelector(".section2");
+const apiText = document.querySelector(".api__text");
+const textdefult = document.querySelector(".defulttext");
+const praentOption = document.querySelector(".nav__options");
 const userInput = document.querySelector(".username");
 const optionNodelist = document.querySelectorAll(".option");
 const userTextground = document.querySelector(".userplayground");
@@ -29,6 +29,9 @@ let randomSentance,
 let apitextArray, Span;
 let apiarrayIndex = 0;
 
+const isMobileDevice = /Mobi/.test(navigator.userAgent);
+console.log(isMobileDevice);
+
 praentOption.addEventListener("click", (e) => {
   // console.log(userInput.value)
   const btn = e.target.dataset.js;
@@ -41,7 +44,10 @@ praentOption.addEventListener("click", (e) => {
     checkclickedtiming = false;
     apitextArray = [];
     clearInterval(timer);
-    userTextground.addEventListener("keydown", checkwrite);
+    userTextground.addEventListener(
+      isMobileDevice ? "touchstart" : "keydown",
+      checkwrite
+    );
     userTextground.value = "";
     apiText.innerText = "";
     for (let j = 0; j < optionArray.length - 1; j++) {
@@ -68,7 +74,10 @@ praentOption.addEventListener("click", (e) => {
       textdefult.classList.add("clsg");
       checkclickedtiming = true;
       timerstate = true;
-      userTextground.addEventListener("keydown", checkwrite);
+      userTextground.addEventListener(
+        isMobileDevice ? "touchstart" : "keydown",
+        checkwrite
+      );
       userTextground.classList.remove("puse");
     }
   }
@@ -93,7 +102,10 @@ function checkwrite() {
     // alert("a")
     Timerstart(timeduration, showTimerin);
   }
-  userTextground.removeEventListener("keydown", checkwrite);
+  userTextground.removeEventListener(
+    isMobileDevice ? "touchstart" : "keydown",
+    checkwrite
+  );
 }
 const keywords = [
   "A",
@@ -231,7 +243,10 @@ function nextlineshow(e) {
   }
 }
 
-userTextground.addEventListener("keydown", nextlineshow);
+userTextground.addEventListener(
+  isMobileDevice ? "touchstart" : "keydown",
+  nextlineshow
+);
 
 // ====== Timer Fuction =====
 function Timerstart(time, place) {
@@ -269,9 +284,6 @@ function Timerstart(time, place) {
     }
   }
 }
-
-
-
 
 // function King(name){
 //         this.name = name;
